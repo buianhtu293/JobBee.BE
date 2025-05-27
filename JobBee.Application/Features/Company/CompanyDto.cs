00b296f38ae687c86754e6916161442f78ea954c
NghiNV <nghinv03@gmail.com>
2025-05-27 14:35:19 +0700

    feat: job dto

---
+++ JobBee.Application/Features/Company/CompanyDto.cs
@@ -0,0 +1,11 @@
+﻿namespace JobBee.Application.Features.Company
+{
+	public class CompanyDto
+	{
+		public Guid Id { get; set; }
+		public string Image { get; set; } = null!;
+		public string Name { get; set; } = null!;
+		public string Location { get; set; } = null!;
+		public int OpenJobs { get; set; } = 0;
+	}
+}
---
+++ JobBee.Application/Features/Job/Queries/GetAllJobs/JobDto.cs
@@ -0,0 +1,16 @@
+﻿using JobBee.Application.Features.Company;
+
+namespace JobBee.Application.Features.Job.Queries.GetAllJobs
+{
+	public class JobDto
+	{
+		public Guid Id { get; set; }
+		public string Title { get; set; } = null!;
+		public string Location { get; set; } = null!;
+		public string JobType { get; set; } = null!;
+		public decimal MinSalary { get; set; }
+		public decimal MaxSalary { get; set; }
+		public bool IsFeatured { get; set; }
+		public CompanyDto Company { get; set; } = null!;
+	}
+}
