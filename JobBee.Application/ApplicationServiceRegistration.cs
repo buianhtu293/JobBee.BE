42340cae88ab4da7192ab934a065a795e574541a
NghiNV <nghinv03@gmail.com>
2025-05-29 22:57:52 +0700

    feat: Config dependency for elastic search

--- JobBee.Application/ApplicationServiceRegistration.cs
+++ JobBee.Application/ApplicationServiceRegistration.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using JobBee.Application.Services;
+using JobBee.Domain.Config;
 using MediatR;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace JobBee.Application
 {
 	public static class ApplicationServiceRegistration
 	{
-		public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+		public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
 		{
 			services.AddAutoMapper(Assembly.GetExecutingAssembly());
 			services.AddMediatR(Assembly.GetExecutingAssembly());
+			services.AddSingleton(typeof(IElasticSearchService<>), typeof(ElasticSearchService<>));
+			services.Configure<ElasticSearchSettings>(configuration.GetSection("ElasticSearchSettings"));
 
 			return services;
 		}
