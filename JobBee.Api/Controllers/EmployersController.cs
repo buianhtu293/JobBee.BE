573c22f8f95a13615523add868b45d980ecc03df
NghiNV <nghinv03@gmail.com>
2025-06-11 21:02:42 +0700

    feat: employer routes

---
+++ JobBee.Api/Controllers/EmployersController.cs
@@ -0,0 +1,17 @@
+﻿using JobBee.Shared.APIRoutes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobBee.Api.Controllers
+{
+	[Route(EmployerRoutes.Index)]
+	[ApiController]
+	public class EmployersController : ControllerBase
+	{
+		//[HttpPost(EmployerRoutes.ACTION.Create)]
+		//public Task<IActionResult> Create([FromForm] )
+		//{
+
+		//}
+	}
+}
