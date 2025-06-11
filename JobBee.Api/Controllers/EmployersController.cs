1ca6702a4a9a145ef475a923186ca5df35117ffc
NghiNV <nghinv03@gmail.com>
2025-06-11 22:07:59 +0700

    feat: employer controller

--- JobBee.Api/Controllers/EmployersController.cs
+++ JobBee.Api/Controllers/EmployersController.cs
@@ -1,5 +1,6 @@
 ﻿using JobBee.Application.Features.Employer.Commands.CreateEmployer;
 using JobBee.Shared.APIRoutes;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,12 +8,15 @@ namespace JobBee.Api.Controllers
 {
 	[Route(EmployerRoutes.Index)]
 	[ApiController]
-	public class EmployersController : ControllerBase
+	public class EmployersController(
+		IMediator mediator
+	) : ControllerBase
 	{
-		//[HttpPost(EmployerRoutes.ACTION.Create)]
-		//public Task<IActionResult> Create([FromForm] CreateEmployerCommand command)
-		//{
-
-		//}
+		[HttpPost(EmployerRoutes.ACTION.Create)]
+		public async Task<IActionResult> Create([FromForm] CreateEmployerCommand command)
+		{
+			var result = await mediator.Send(command);
+			return Created();
+		}
 	}
 }
