32470518abc305f652d19384c6c77f3bf1c8fc30
NghiNV <nghinv03@gmail.com>
2025-06-12 22:57:09 +0700

    feat: add job validator pipeline

---
+++ JobBee.Application/Features/Job/Commands/CreateJob/CreateJobCommandValidator.cs
@@ -0,0 +1,57 @@
+﻿using FluentValidation;
+
+public class CreateJobCommandValidator : AbstractValidator<CreateJobCommand>
+{
+	public CreateJobCommandValidator()
+	{
+		RuleFor(x => x.EmployerId).NotEmpty();
+		RuleFor(x => x.Title)
+			.NotEmpty()
+			.MaximumLength(100);
+
+		RuleFor(x => x.JobCategoryId).NotEmpty();
+		RuleFor(x => x.JobTypeId).NotEmpty();
+		RuleFor(x => x.ExperienceLevelId).NotEmpty();
+		RuleFor(x => x.MinEducationId).NotEmpty();
+
+		RuleFor(x => x.Description)
+			.NotEmpty()
+			.MinimumLength(30);
+
+		RuleFor(x => x.Responsibilities)
+			.NotEmpty()
+			.MinimumLength(10);
+
+		RuleFor(x => x.Requirements)
+			.NotEmpty()
+			.MinimumLength(10);
+
+		RuleFor(x => x.MinSalary)
+			.GreaterThanOrEqualTo(0);
+
+		RuleFor(x => x.MaxSalary)
+			.GreaterThanOrEqualTo(x => x.MinSalary);
+
+		RuleFor(x => x.SalaryPeriod)
+			.NotEmpty()
+			.Must(x => x == "Monthly" || x == "Yearly" || x == "Hourly")
+			.WithMessage("SalaryPeriod must be Monthly, Yearly, or Hourly.");
+
+		RuleFor(x => x.Currency)
+			.NotEmpty()
+			.Length(2, 5); // e.g., USD, EUR
+
+		RuleFor(x => x.LocationCity)
+			.NotEmpty();
+
+		RuleFor(x => x.LocationState)
+			.NotEmpty();
+
+		RuleFor(x => x.LocationCountry)
+			.NotEmpty();
+
+		RuleFor(x => x.ApplicationDeadline)
+			.GreaterThan(0)
+			.WithMessage("ApplicationDeadline must be a valid timestamp.");
+	}
+}
