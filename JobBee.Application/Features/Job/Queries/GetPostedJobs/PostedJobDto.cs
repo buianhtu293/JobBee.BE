392ee7b35c1888f40b7594cb3df355c44c5f8f0a
NghiNV <nghinv03@gmail.com>
2025-06-14 09:44:02 +0700

    feat: get posted jobs

---
+++ JobBee.Application/Features/Job/Queries/GetPostedJobs/PostedJobDto.cs
@@ -0,0 +1,12 @@
+﻿namespace JobBee.Application.Features.Job.Queries.GetPostedJobs
+{
+	public class PostedJobDto
+	{
+		public Guid Id { get; set; }
+		public string Title { get; set; } = null!;
+		public string JobType { get; set; } = null!;
+		public int DaysRemaing { get; set; }
+		public bool IsActive { get; set; }
+		public int Application { get; set; }
+	}
+}
