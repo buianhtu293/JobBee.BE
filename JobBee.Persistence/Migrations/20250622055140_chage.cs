aed13bee72f5c535965bdd84136f9c1ebe111386
NghiNV <nghinv03@gmail.com>
2025-06-22 12:52:51 +0700

    fix: update column type

---
+++ JobBee.Persistence/Migrations/20250622055140_chage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobBee.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class chage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "expires_at",
+                table: "jobs",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "application_deadline",
+                table: "jobs",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "expires_at",
+                table: "jobs",
+                type: "timestamp without time zone",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "application_deadline",
+                table: "jobs",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+        }
+    }
+}
