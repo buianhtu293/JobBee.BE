10ef2c156b36d3b4cd95064c033f020ab05d4523
NghiNV <nghinv03@gmail.com>
2025-06-29 22:46:51 +0700

    fix: job detail

--- JobBee.Persistence/Repositories/JobRepository.cs
+++ JobBee.Persistence/Repositories/JobRepository.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using JobBee.Application.Contracts.Persistence;
+using JobBee.Application.Exceptions;
+using JobBee.Application.Features.Employer.Commands.CreateEmployer;
+using JobBee.Application.Features.Job.Queries.GetDetail;
 using JobBee.Domain.Entities;
 using JobBee.Persistence.DatabaseContext;
 using JobBee.Shared.Paginators;
+using Microsoft.EntityFrameworkCore;
 
 namespace JobBee.Persistence.Repositories
 {
@@ -37,6 +41,60 @@ namespace JobBee.Persistence.Repositories
 				e => e.ExperienceLevel,
 				e => e.MinEducation
 				);
+		}
+
+		public async Task<JobDetailDto> GetJobDetail(Guid jobId, CancellationToken cancellationToken)
+		{
+			var job = await _context.jobs
+				.Include(j => j.Employer)
+					.ThenInclude(e => e.EmployerSocialMedia)
+				.Include(j => j.Employer)
+					.ThenInclude(e => e.Industry)
+				.Include(j => j.Employer)
+					.ThenInclude(e => e.CompanySize)
+				.Include(j => j.JobType)
+				.Include(j => j.ExperienceLevel)
+				.Include(j => j.MinEducation)
+				.FirstOrDefaultAsync(x => x.Id == jobId);
+
+			if(job == null) throw new NotFoundException(nameof(job), jobId);
+
+			var jobDetailDto = new JobDetailDto
+			{
+				Title = job.Title,
+				Description = job.Description,
+				Responsibility = job.Responsibilities!,
+				PostedAt = job.PostedAt!.Value,
+				ApplicationDeadline = job.ApplicationDeadline!.Value,
+				Level = job.MinEducation!.LevelName,
+				MinSalary = job.MinSalary!.Value,
+				MaxSalary = job.MaxSalary!.Value,
+				LocationCity = job.LocationCity!,
+				JobType = job.JobType!.TypeName,
+				Experience = job.ExperienceLevel!.LevelName,
+				Employer = new Application.Features.Employer.Queries.GetEmployerDetail.EmployerDetailDTO()
+				{
+					CompanyLogo = job.Employer.CompanyLogo!,
+					CompanyName = job.Employer.CompanyName,
+					CompanySize = job.Employer.CompanySize!.SizeRange,
+					ContactEmail = job.Employer.ContactEmail!,
+					ContactPhone = job.Employer.ContactPhone!,
+					Industry = job.Employer.Industry!.IndustryName,
+					WebsiteUrl = job.Employer.WebsiteUrl!,
+					SocialMedials = new List<SocialMedial>()
+				}
+			};
+
+			foreach(var link in job.Employer.EmployerSocialMedia)
+			{
+				jobDetailDto.Employer.SocialMedials.Add(new SocialMedial
+				{
+					Link = link.ProfileUrl,
+					Platform = link.PlatformName,
+				});
+			}
+
+			return jobDetailDto;
 		}
 
 		public async Task<PageResult<Job>> GetJobsAppliedByCandidateAsync(Guid candidateId, int pageIndex, int pageSize, CancellationToken cancellationToken)
